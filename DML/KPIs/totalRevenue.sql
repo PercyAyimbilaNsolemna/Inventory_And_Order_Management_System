3c1fd94b829276f25393a829cdc9c619899019a3
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2025-12-24 09:49:38 +0000

    Updated the total revenue sql script.

--- DML/KPIs/totalRevenue.sql
+++ DML/KPIs/totalRevenue.sql
@@ -1,3 +1,9 @@
+/*
+
+    Calculates the total revenue for shipped or delivered orders
+
+*/
+
 SELECT SUM(TotalAmount) AS `Total Revenue`
     FROM `Orders`
     WHERE CurrentStatus IN ('Delivered', 'Shipped')
