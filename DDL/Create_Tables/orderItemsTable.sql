677f39af5d66f6f6c0baa7440b40dde7f1ec51a3
PercyAyimbilaNsolemna <percy.nsolebna@gmail.com>
2025-12-25 15:59:15 +0000

    Removed the SKU field from the order items table and updated the data inserted into the order items table to exclude SKU.

--- DDL/Create_Tables/orderItemsTable.sql
+++ DDL/Create_Tables/orderItemsTable.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS `Order_Items` (
   `OrderID` INT NOT NULL,
   `ProductID` INT NOT NULL,
   `Quantity` INT NOT NULL CHECK (Quantity > 0),
-  `SKU` VARCHAR(50),
   `PriceAtPurchase` DECIMAL(10,2) NOT NULL,
   `SubTotal` DECIMAL(10,2) NOT NULL,
   FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
